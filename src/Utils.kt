05b386e85c5b052a6c7a23f34b983415384d6d11
mmurphy-figure <mmurphy@figure.com>
2021-11-30 10:41:46 -0800

    add more utils

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,6 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+// byte array to hex string
+fun ByteArray.toHexString() = joinToString("") { (0xFF and it.toInt()).toString(16).padStart(2, '0') }
